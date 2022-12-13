97c43a0c69659e15dd146c912c15d604a4fe6a78
Jaewe Heo <jaeweheo@gmail.com>
2022-12-13 23:33:51 +0900

    Set displays to 0.1.0

--- Formula/displays.rb
+++ Formula/displays.rb
@@ -1,8 +1,8 @@
 class Displays < Formula
   desc "Get information of macOS displays from the command-line"
   homepage "https://github.com/importre/macos-displays"
-  url "https://github.com/importre/macos-displays/releases/download/0.0.0/displays"
-  sha256 "97bef8d5d328bb61cd2fb9bac96347c39d4fe2ad7226397272a71bcdcbbbd32e"
+  url "https://github.com/importre/macos-displays/releases/download/0.1.0/displays"
+  sha256 "4e6fedd88b8488f181620424f95d97b5e89bc58b97cce9f4c2c4a4083e68eb0c"
   license "MIT"
 
   depends_on macos: :monterey
